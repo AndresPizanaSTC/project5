bdcbff68cd02fb79926526f1a202960f0e8315c7
Andres Pizana <apizana1@stu.southtexascollege.edu>
2025-03-02 20:10:56 -0600

    changed a few tiny things

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav class="bg-purple-600 text-white p-4 flex justify-between">
-    <h1 class="text-xl font-bold">Anime Explorer</h1>
+  <nav class="bg-purple-600 text-white p-6 flex justify-between">
+    <h1 class="text-xl font-bold">Anime Cards (Andres Pizana)</h1>
     <div>
       <router-link to="/" class="mr-4 hover:underline">Home</router-link>
       <router-link to="/login" class="hover:underline">Login</router-link>
